40ffa8d86d2ebc90f4e114200c0c0edb35302117
Yanti <wsutr@hotmail.com>
2023-04-20 16:37:54 -0600

    blogslist backend exercise 4.1-4.2

---
+++ controllers/blogs.js
@@ -0,0 +1,31 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (req, res, next)=>{
+    Blog
+    .find({})
+    .then(blogs => {
+        res.json(blogs)
+    })
+    .catch(error => next(error))
+})
+blogsRouter.post('/', (req, res, next)=>{
+    const body = req.body
+    // needs to add if body content is empty
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+
+    })
+
+    blog
+    .save()
+    .then(result => {
+        res.status(201).json(result)
+    })
+    .catch(error => nextTick(error))
+})
+
+module.exports = blogsRouter
---
+++ models/blog.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+// remove the mongoose styled id and v
+blogSchema.set("toJSON", {
+    transform: (document, returnObject) =>{
+        returnObject.id = returnObject._id.toString()
+        delete returnObject._id
+        delete returnObject.__v
+    }
+})
+module.exports = mongoose.model('Blog', blogSchema)
